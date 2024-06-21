7b177fdde7292718a6f37145b8f43db8735ccf84
winnier21 <you@example.com>
2024-06-21 15:09:21 -0700

    endpoints test and update

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ import videosRoute from './routes/videos.js'
 
 const app = express()
 
+
 app.use(express.json())
 app.use(cors())
-app.use('/videos', videosRoute)
+app.use('/videos', route)
 app.use(express.static('public'))
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8082
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
 })
--- routes/videos.js
+++ routes/videos.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import fs from 'fs';
-import {v4, uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 
-const router = express. Router();
+const router = express.Router();
 
-
+// Middleware to parse JSON
+router.use(express.json());
 //GET /videos
 router.get ('/', (req, res) => {
     const videoData = JSON.parse (fs.readFileSync ('./data/video.json'))
@@ -21,18 +22,24 @@ router.get ('/', (req, res) => {
 //GET /videos/:id
 router.get ('/:videoId', (req,res)=>{
     const {videoId}= req.params
+    try {
     const videoData = JSON.parse (fs.readFileSync('./data/video.json'))
-    const videoDetails = videos.find ((video)=> video.id === videoId)
+    const videoDetails = videoData.find ((video)=> video.id === videoId)
     
     if (!videoDetails){
         return res.status (404).send ("Sorry, Video not found");
     }
     res.json (videoDetails);
+    }catch (error){
+        console.log ('error', error);
+        res.status(500).send('Error reading video data file');
+    }
+    
 });
 
 //POST /VIDEOS
 router.post ('/', (req, res) =>{
-    const videoData = JSON.parse(fs.readFileSync('./data/videos.json'))
+    const videoData = JSON.parse(fs.readFileSync('./data/video.json'))
     const { title, description } = req.body;
     const newVideo = {
         id: uuidv4(),
@@ -42,18 +49,18 @@ router.post ('/', (req, res) =>{
         description,
         views: 0,
         likes: 0,
-        duration,
+        duration: "0:00",
         video: "https://unit-3-project-api-0a5620414506.herokuapp.com/stream",
         timestamp: Date.now(),
         comments: []
     };
     videoData.push (newVideo);
-    fs.writeFileSync("./data/videos.json", JSON.stringify(videoData));
+    fs.writeFileSync("./data/video.json", JSON.stringify(videoData));
     res.status(201).json({message: 'Thank you for uploading a video'})
 })
 
 router.post('/:videoId/comments', (req, res) => {
-    const videoData = JSON.parse(fs.readFileSync('./data/videos.json'))
+    const videoData = JSON.parse(fs.readFileSync('./data/video.json'))
     const videoId = req.params.videoId
 
     const newComments = {
