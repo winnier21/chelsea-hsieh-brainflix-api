ab64a23f8a8cc9ab3f727923623c91cf56cfb9b9
winnier21 <you@example.com>
2024-06-23 14:27:20 -0700

    delected unuse comments

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-import express from "express"
-import 'dotenv/config'
-import cors from 'cors'
-import route from './routes/videos.js'
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import route from "./routes/videos.js";
 
-const app = express()
+const app = express();
 
-
-app.use(express.json())
-// const CORS_ORIGIN = process.env.CROSS_ORIGIN || 'http://localhost:5174';
+app.use(express.json());
 app.use(cors());
-app.use('/videos', route)
-app.use(express.static('public'))
+app.use("/videos", route);
+app.use(express.static("public"));
 
-const PORT = process.env.PORT || 8080
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
